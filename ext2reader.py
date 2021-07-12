af1343bb70442ee36b7547870cd4cb7762c3f564
Freddy <frederik@bussmann.io>
2021-07-12 17:05:13 +0200

    Added instructions for image extraction

--- ext2reader.py
+++ ext2reader.py
@@ -9,5 +9,13 @@ ext2file.read("data/", "agwc.txt")
 
 ext2file.print_statistics()
 
-# img = Image.open(io.BytesIO(ext2file.data_blocks))
-# img.save("data/out.jpg")
+
+# Extract agwc.jpg from ext2file
+
+first_inode_index = ext2file.super_block.s_first_ino  # Get block index of first inode
+first_inode = ext2file.inodes[first_inode_index]  # Get first inode via their block index
+
+data_blocks = ext2file.get_inode_blocks(first_inode)  # Get data blocks from first inode
+
+img = Image.open(io.BytesIO(data_blocks))  # Save bytes from data_blocks into image file
+img.save("data/agwc.jpg")  # Save image file at path/name
